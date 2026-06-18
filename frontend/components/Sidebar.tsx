0d6b2cd9be2525a292a8f88ff79c68dd82117274
Yassine JamiL <yassine.jamil@example.com>
2026-06-18 20:25:01 +0100

    Add static sidebar component

---
+++ frontend/components/Sidebar.tsx
@@ -0,0 +1,16 @@
+export default function Sidebar() {
+  return (
+    <aside>
+      <h2>P2P Wallet</h2>
+
+      <ul>
+        <li>Dashboard</li>
+        <li>Send Money</li>
+        <li>History</li>
+        <li>Ledger</li>
+        <li>Profile</li>
+        <li>Settings</li>
+      </ul>
+    </aside>
+  );
+}
